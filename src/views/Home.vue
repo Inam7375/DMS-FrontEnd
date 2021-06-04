ea7c955e76cbbefdb3e5f004e1f30f47bb760a50
Inam7375 <inam.ali211.ia@gmail.com>
2021-06-04 15:58:31 +0500

    Home Page Tracking Included

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,16 +29,119 @@
 					<p style="color:white" class="text-2xl subpixel-antialiased">Forwarded Documents</p>
 				</vx-card>
 			</b-col>
-		</b-row>
-		<h4>You are in home.</h4>
+		</b-row><br><br>
+		<vx-card >
+			<p class="text-3xl">All Documents</p>
+			<div class="con-tab-ejemplo">
+				<vs-table max-items="5" search pagination :data="all_documents">
+					<div slot="thead" class="grid grid-cols-8 gap-4">
+					<vs-th sort-key="_id" style="flex-grow:1">
+						Document ID
+					</vs-th>
+					<vs-th sort-key="title" style="flex-grow:1">
+						Title
+					</vs-th>
+					<vs-th sort-key="created_by_user" style="flex-grow:1"> 
+						Created By
+					</vs-th>
+					<vs-th  sort-key="created_by_department" style="flex-grow:1"> 
+						From Dep
+					</vs-th>
+					<vs-th sort-key="target_user" style="flex-grow:1">
+						Forwarded To  
+					</vs-th>
+					<vs-th sort-key="target_department" style="flex-grow:1">
+						Forwarded Dep  
+					</vs-th>
+					<vs-th sort-key="target_department" style="flex-grow:1">
+						Date Created  
+					</vs-th>
+					<vs-th sort-key="role" style="flex-grow:1">
+						Actions  
+					</vs-th>
+					</div>
+
+					<div slot-scope="{data}">
+					<vs-tr :state="tr.role == 'Super Admin'?'success':tr.role == 'Admin'?'primary':null" :key="indextr" v-for="(tr, indextr) in data" class="grid grid-cols-8 gap-4">
+						<vs-td :data="data[indextr]._id">
+						{{data[indextr]._id}}
+						</vs-td>
+						<vs-td :data="data[indextr].title">
+						{{data[indextr].title}}
+						</vs-td>
+
+						<vs-td :data="tr.created_by_user">
+						{{tr.created_by_user}}
+						</vs-td>
+
+						<vs-td :data="tr.created_by_department">
+						{{tr.created_by_department}} 
+						</vs-td>
+
+						<vs-td :data="tr.target_user">
+						{{tr.target_user}}
+						</vs-td>
+
+						<vs-td :data="tr.target_department">
+						{{tr.target_department}}
+						</vs-td>
+
+						<vs-td :data="tr.date_created">
+						{{tr.date_created}}
+						</vs-td>
+						<vs-td>
+							<div>
+							<router-link :to="{name: 'document', params: {docID: tr._id}}">
+								<vs-button @click="toggleCreator" radius size="large" line-position="left" color="success" type="flat" icon="edit"></vs-button>
+							</router-link>
+							</div>
+						</vs-td>
+					</vs-tr>
+					</div>
+				</vs-table>
+			</div>
+		</vx-card>
 	</div>
 </template>
 
 <script>
 import VxCard from '../components/vx-card/VxCard.vue'
+import VueJwtDecode from 'vue-jwt-decode';
+import axios from 'axios';
+
 export default{
 	name: 'Home',
+	data () {
+		return {
+			isAdmin : false,
+			value1: "",
+			payloadUname: "",
+			all_documents: []
+
+		}
+	},
   	components: { VxCard },
+	methods : {
+		get_all_documents: async function() {
+			const payload = VueJwtDecode.decode(localStorage.getItem('access-token'))
+			if (this.isAdmin == true){
+				const res = await axios.get('http://localhost:5000/api/documents')
+				this.all_documents = res.data.results
+			}else{
+				this.payloadUname = payload['username']
+				const res = await axios.get(`http://localhost:5000/api/userdocuments/${this.payloadUname}`)
+				this.all_documents = res.data.results
+			}
+		}
+	},
+	created() {
+		if (localStorage.getItem('isAdmin') == "true"){
+			this.isAdmin = true
+		}else{
+			this.isAdmin = false
+		}
+		this.get_all_documents()
+	}
 }
 </script>
 
